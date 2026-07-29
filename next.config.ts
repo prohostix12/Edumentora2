bb17d6ce768316c47f271e2a6585b186f6264f94
Abheesh <abheeshkumaran7@gmail.com>
2026-07-29 16:29:19 +0530

    onnulla

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // @ts-expect-error: New feature in Next.js 15+ not yet in types
   allowedDevOrigins: ['192.168.1.33'],
   experimental: {
     serverActions: {
